34322ecd7a53707df2a8b2facb4e0810585ca416
Livia-Gomes <livia@momentum-free.com>
2022-05-04 21:06:44 +0100

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // ⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈⏈
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+}; 
 Object.defineProperty(exports, "__esModule", { value: true });
 // To keep the console relatively clean, you may
 // comment previous exercises as you progress.
